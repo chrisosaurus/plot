bd55c6ba96b8c005cf3486b92d7cbbc9fe64384b
Chris Hall <followingthepath@gmail.com>
2013-08-28 21:41:28 +1200

    environment starting, added fixme notes to variable

--- src/variable.h
+++ src/variable.h
@@ -3,6 +3,14 @@
 
 #include "value.h"
 
+/* FIXME should plot_value be a pointer or a value?
+ * do we want to share and have proper ref counting?
+ * is it needed in such a language?
+ * it is possible for 2 names to refer to the same value
+ * e.g. when passing a variable as an argument we are really
+ * giving the same value a new name (as the old name can be
+ * assigned to a new value)
+ */
 typedef struct plot_variable {
     char *name;
     plot_value *val;
