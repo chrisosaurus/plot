91204fca448003db643c9b8c4c582864a4c1daa6
Chris Hall <followingthepath@gmail.com>
2013-09-26 21:20:34 +1200

    added env implementation, still need to write plot_hash_set to complete env

--- src/env.c
+++ src/env.c
@@ -1,3 +1,5 @@
+#include <stdlib.h> /* calloc, free */
+
 #include "env.h"
 
 /* allocated a new plot_env using calloc
@@ -6,11 +8,24 @@
  * returns pointer to new env or 0 on error
  */
 plot_env * plot_env_init(plot_env *parent){
-    return 0;
+    plot_env *e = calloc(1, sizeof *e);
+    if( ! e )
+        return 0;
+
+    e->hash = plot_hash_init();
+    if( ! e->hash )
+        return 0;
+
+    return e;
 }
 
 /* free env and calls hash cleanup */
 void plot_env_cleanup(plot_env *env){
+    if( ! env )
+        return;
+
+    plot_hash_cleanup(env->hash);
+    free(env);
 }
 
 /* resolve a symbol to a value
@@ -19,6 +34,20 @@ void plot_env_cleanup(plot_env *env){
  * return plot_value* or 0 when symbol is not found
  */
 plot_value * plot_env_get(plot_env *env, plot_symbol * const sym){
+    plot_env *e;
+    plot_value *v;
+
+    if( ! sym )
+        return 0;
+
+    for( e=env; e; e=e->parent ){
+        if( ! e->hash )
+            break;
+        v = plot_hash_get(e->hash, sym);
+        if( v )
+            return v;
+    }
+
     return 0;
 }
 
@@ -29,7 +58,12 @@ plot_value * plot_env_get(plot_env *env, plot_symbol * const sym){
  * returns 1 on success, 0 on error
  */
 int plot_env_define(plot_env *env, plot_symbol * const sym, plot_value * const val){
-    return 0;
+    if( ! env || ! sym || ! val )
+        return 0;
+    if( ! env->hash )
+        return 0;
+
+    return plot_hash_insert(env->hash, sym, val);
 }
 
 /* set a a symbol to a value, it may or may not already exist within this env
@@ -37,7 +71,12 @@ int plot_env_define(plot_env *env, plot_symbol * const sym, plot_value * const v
  * returns 1 on success, 0 on error
  */
 int plot_env_set(plot_env *env, plot_symbol * const sym, plot_value * const val){
-    return 1;
+    if( ! env || ! sym || ! val )
+        return 0;
+    if( ! env->hash )
+        return 0;
+
+    return plot_hash_set(env->hash, sym, val);
 }
 
 
