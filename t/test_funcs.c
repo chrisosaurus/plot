219149a891d59d077c8659027e00fe6b51bb40a9
Chris Hall <followingthepath@gmail.com>
2013-10-01 20:57:23 +1300

    adding more comprehensive tests to test_funcs

--- t/test_funcs.c
+++ t/test_funcs.c
@@ -37,6 +37,43 @@ START_TEST (test_funcs_add){
 }
 END_TEST
 
+START_TEST (test_funcs_env){
+    plot_symbol sym;
+    plot_value add;
+    plot_env *env = plot_env_init(0);
+
+    plot_value *res, v1, v2;
+    plot_value *f;
+
+    sym.val = "+";
+    sym.len = 2;
+    sym.size = 2;
+
+    add.type = plot_type_function;
+    add.u.function.func = plot_func_add;
+    puts("\tdefining function add");
+    fail_unless( 1 == plot_env_define(env, &sym, &add) );
+
+    v1.type = plot_type_number;
+    v1.u.number.val = 4;
+
+    v2.type = plot_type_number;
+    v2.u.number.val = 5;
+    puts("\ttesting fetching function");
+    fail_if( 0 == (f = plot_env_get(env, &sym)) );
+    fail_unless( f->type == plot_type_function );
+    fail_unless( f->u.function.func == plot_func_add );
+
+    puts("\ttesting function call");
+    fail_if( 0 == (res = f->u.function.func(env, &v1, &v2)) );
+    fail_unless( res->type == plot_type_number );
+    fail_unless( res->u.number.val == 9 );
+
+    plot_env_cleanup(env);
+    free(res);
+}
+END_TEST
+
 Suite *
 funcs_suite(void){
     Suite *s = suite_create("suite_funcs");
@@ -44,6 +81,7 @@ funcs_suite(void){
     TCase *tc_funcs = tcase_create("test_funcs");
 
     tcase_add_test(tc_funcs, test_funcs_add);
+    tcase_add_test(tc_funcs, test_funcs_env);
 
     suite_add_tcase(s, tc_funcs);
 
