7143c8259397f6c13f7923558d7185e94f11e39c
Chris Hall <followingthepath@gmail.com>
2013-10-21 21:31:54 +1300

    adding some more debugging information for hash.c

--- src/hash.c
+++ src/hash.c
@@ -6,7 +6,7 @@
 #include "plot.h"
 
 #define DEBUG 0
-#define DEBUG_CLEANUP 1
+#define DEBUG_CLEANUP 0
 
 /* create a new hash
  * allocate a new hash using calloc
@@ -46,7 +46,9 @@ void plot_hash_cleanup(plot_hash *hash){
     for( cur = hash->head; cur; cur = nxt ){
         nxt = cur->next;
         #if DEBUG || DEBUG_CLEANUP
-        printf("\tdecreasing value '%p' with refcount '%d'\n", (void*) cur->value, cur->value->gc.refcount);
+        if( cur->value ){
+            printf("\tdecreasing value '%p' with refcount '%d'\n", (void*) cur->value, cur->value->gc.refcount);
+        }
         #endif
         plot_value_decr(cur->value);
         /* FIXME need to deref each hash_entry */
@@ -83,7 +85,9 @@ plot_value * plot_hash_get(plot_hash *hash, const plot_symbol * key){
         if( ! strcmp(key->val, e->key->val) ){
             plot_value_incr(e->value);
             #if DEBUG || DEBUG_CLEANUP
-            printf("\treturning found object '%s' ('%p') with refcount '%d'\n", e->key->val,(void*) e->value, e->value->gc.refcount);
+            if( e->value ){
+                printf("\treturning found object '%s' ('%p') with refcount '%d'\n", e->key->val,(void*) e->value, e->value->gc.refcount);
+            }
             #endif
             return e->value;
         }
