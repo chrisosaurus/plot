fa73977ce75aebe9ed23fc59d6c5c62ef476541b
Chris Hall <followingthepath@gmail.com>
2013-11-03 10:59:40 +1300

    Breaking number functions into separate files

--- src/funcs.h
+++ src/funcs.h
@@ -7,6 +7,7 @@ struct plot_expr;
 
 #include "character.h"
 #include "string.h"
+#include "number.h"
 
 /* print value to stdout
  */
@@ -15,60 +16,6 @@ struct plot_value * plot_func_display(struct plot_env *env, struct plot_value **
 /* print a newline to stdout
  */
 struct plot_value * plot_func_newline(struct plot_env *env, struct plot_value **args, int argc);
-
-/***** mathmatical functions ******/
-
-/* takes a list of expressions
- * evals each and then adds the expression's value if it is a number
- * if any of the expressions evaluate to something other than an error
- * throw plot_error_bad_args
- */
-struct plot_value * plot_func_add(struct plot_env *env, struct plot_value **args, int argc);
-
-/* takes a list of expressions
- * evals each and then subtracts the expression's value if it is a number
- * if any of the expressions evaluate to something other than an error
- * throw plot_error_bad_args
- */
-struct plot_value * plot_func_subtract(struct plot_env *env, struct plot_value **args, int argc);
-
-/* takes a list of expressions
- * evals each and then multiplies the expression's value if it is a number
- * if any of the expressions evaluate to something other than an error
- * throw plot_error_bad_args
- */
-struct plot_value * plot_func_multiply(struct plot_env *env, struct plot_value **args, int argc);
-
-/* integer division
- * exact only
- */
-struct plot_value * plot_func_divide(struct plot_env *env, struct plot_value **args, int argc);
-
-/* remainder
- */
-struct plot_value * plot_func_remainder(struct plot_env *env, struct plot_value **args, int argc);
-
-/******** comparison functions *******/
-
-/* mathmatical =
- */
-struct plot_value * plot_func_math_equal(struct plot_env *env, struct plot_value **args, int argc);
-
-/* <
- */
-struct plot_value * plot_func_less(struct plot_env *env, struct plot_value **args, int argc);
-
-/* >
- */
-struct plot_value * plot_func_greater(struct plot_env *env, struct plot_value **args, int argc);
-
-/* <=
- */
-struct plot_value * plot_func_less_equal(struct plot_env *env, struct plot_value **args, int argc);
-
-/* >=
- */
-struct plot_value * plot_func_greater_equal(struct plot_env *env, struct plot_value **args, int argc);
 
 /********* equivalent predicates *********/
 /* equal?
---
+++ src/number.h
@@ -0,0 +1,62 @@
+#ifndef PLOT_NUMBER_H
+#define PLOT_NUMBER_H
+
+struct plot_env;
+struct plot_value;
+/***** mathmatical functions ******/
+
+/* takes a list of expressions
+ * evals each and then adds the expression's value if it is a number
+ * if any of the expressions evaluate to something other than an error
+ * throw plot_error_bad_args
+ */
+struct plot_value * plot_func_add(struct plot_env *env, struct plot_value **args, int argc);
+
+/* takes a list of expressions
+ * evals each and then subtracts the expression's value if it is a number
+ * if any of the expressions evaluate to something other than an error
+ * throw plot_error_bad_args
+ */
+struct plot_value * plot_func_subtract(struct plot_env *env, struct plot_value **args, int argc);
+
+/* takes a list of expressions
+ * evals each and then multiplies the expression's value if it is a number
+ * if any of the expressions evaluate to something other than an error
+ * throw plot_error_bad_args
+ */
+struct plot_value * plot_func_multiply(struct plot_env *env, struct plot_value **args, int argc);
+
+/* integer division
+ * exact only
+ */
+struct plot_value * plot_func_divide(struct plot_env *env, struct plot_value **args, int argc);
+
+/* remainder
+ */
+struct plot_value * plot_func_remainder(struct plot_env *env, struct plot_value **args, int argc);
+
+
+/******** comparison functions *******/
+
+/* mathmatical =
+ */
+struct plot_value * plot_func_math_equal(struct plot_env *env, struct plot_value **args, int argc);
+
+/* <
+ */
+struct plot_value * plot_func_less(struct plot_env *env, struct plot_value **args, int argc);
+
+/* >
+ */
+struct plot_value * plot_func_greater(struct plot_env *env, struct plot_value **args, int argc);
+
+/* <=
+ */
+struct plot_value * plot_func_less_equal(struct plot_env *env, struct plot_value **args, int argc);
+
+/* >=
+ */
+struct plot_value * plot_func_greater_equal(struct plot_env *env, struct plot_value **args, int argc);
+
+
+#endif
