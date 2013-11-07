14ef621208fe214e19e701b45d01576cc5857462
Chris Hall <followingthepath@gmail.com>
2013-11-07 21:43:19 +1300

    Updating pair to use new api

--- src/pair.c
+++ src/pair.c
@@ -10,38 +10,26 @@
  * return #t iff obj is of type plot_type_pair
  */
 struct plot_value * plot_func_pair_test(struct plot_env *env, struct plot_value **args, int argc){
-    plot_value *res;
-
     if( argc != 1 ){
         return plot_runtime_error(plot_error_bad_args, "expected exactly 1 argument", "plot_func_pair_test");
     }
 
-    res = plot_alloc_value();
-    res->type = plot_type_boolean;
-    res->u.boolean.val = args[0]->type == plot_type_pair;
-    return res;
+    return plot_new_boolean( args[0]->type == plot_type_pair );
 }
 
 /* (cons v1 v2)
  * return a pair with v1 as car and v2 as cdr
  */
 struct plot_value * plot_func_pair_cons(struct plot_env *env, struct plot_value **args, int argc){
-    plot_value *res;
-
     if( argc != 2 ){
         return plot_runtime_error(plot_error_bad_args, "expected exactly 2 arguments", "plot_func_pair_cons");
     }
 
-    res = plot_alloc_value();
-    res->type = plot_type_pair;
-
-    res->u.pair.car = args[0];
     plot_value_incr(args[0]);
 
-    res->u.pair.cdr = args[1];
     plot_value_incr(args[1]);
 
-    return res;
+    return plot_new_pair(args[0], args[1]);
 }
 
 /* (car obj)
