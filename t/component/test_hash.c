49ebdb2568aaabd6647e5f2b6ec657a5d47a00a3
Chris Hall <followingthepath@gmail.com>
2015-08-06 14:41:31 +1200

    updating test_hash

--- t/component/test_hash.c
+++ t/component/test_hash.c
@@ -18,7 +18,6 @@ START_TEST (test_hash){
 
     plot_value value;
     plot_hash hash;
-    hash.n_elems = 0;
     value.gc.refcount = 1;
 
     puts("\tTesting hash construction");
@@ -31,21 +30,13 @@ START_TEST (test_hash){
     fail_if( plot_hash_get(&hash, &b) != &value );
     fail_if( plot_hash_get(&hash, &a) );
 
-    puts("\tTesting for correct key ordering");
-    fail_unless( plot_hash_set(&hash, &a, &value) );
-    fail_unless( plot_hash_set(&hash, &c, &value) );
-    fail_if( strcmp(a.val, hash.head->key->val) );
-    fail_if( strcmp(b.val, hash.head->next->key->val) );
-    fail_if( strcmp(c.val, hash.head->next->next->key->val) );
-
     puts("\tTesting correct number of elements");
-    fail_unless( hash.head->next->next->next == 0 );
-    fail_unless( hash.n_elems == 3);
+    fail_unless( plot_hash_nelems(&hash) == 3);
 
     puts("\tTesting mutation");
     fail_unless( plot_hash_set(&hash, &b, 0) );
     fail_unless( 0 == plot_hash_get(&hash, &b) );
-    fail_unless( 3 == hash.n_elems );
+    fail_unless( 3 == plot_hash_nelems(&hash) );
 
     plot_hash_cleanup(&hash);
     plot_cleanup();
